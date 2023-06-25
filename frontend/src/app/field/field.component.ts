f223092236f70f18589d2b965ac37fd74ed67b3b
benni043 <benni.huff@gmail.com>
2023-06-25 20:39:58 +0200

    mobile

--- frontend/src/app/field/field.component.ts
+++ frontend/src/app/field/field.component.ts
@@ -10,103 +10,117 @@ import {PointsField} from "../game";
 export class FieldComponent implements OnInit{
 
   constructor(private routerService: RouterService) {
-    // this.map = new Map();
-    //
-    // this.map.set("test", {
-    //   doubleGrande: 0,
-    //   fives: 0,
-    //   fours: 0,
-    //   fullHouse: 0,
-    //   grande: 0,
-    //   ones: 0,
-    //   poker: 0,
-    //   sixes: 0,
-    //   street: 0,
-    //   sum: 0,
-    //   threes: 0,
-    //   twos: 0
-    // })
-    //
-    // this.map.set("test2", {
-    //   doubleGrande: 0,
-    //   fives: 0,
-    //   fours: 0,
-    //   fullHouse: 0,
-    //   grande: 0,
-    //   ones: 0,
-    //   poker: 0,
-    //   sixes: 0,
-    //   street: 0,
-    //   sum: 0,
-    //   threes: 0,
-    //   twos: 123
-    // })
+    this.map = new Map();
+
+    this.map.set("test", {
+      doubleGrande: 100,
+      fives: 0,
+      fours: 3,
+      fullHouse: 0,
+      grande: 0,
+      ones: 100,
+      poker: 0,
+      sixes: 0,
+      street: 0,
+      sum: 0,
+      threes: 0,
+      twos: 100
+    })
+
+    this.map.set("test2", {
+      doubleGrande: 0,
+      fives: 0,
+      fours: 0,
+      fullHouse: 0,
+      grande: 0,
+      ones: 1,
+      poker: 0,
+      sixes: 0,
+      street: 0,
+      sum: 0,
+      threes: 0,
+      twos: 123
+    })
+
+    this.map.set("test3", {
+      doubleGrande: 0,
+      fives: 0,
+      fours: 0,
+      fullHouse: 0,
+      grande: 0,
+      ones: 1,
+      poker: 0,
+      sixes: 0,
+      street: 0,
+      sum: 0,
+      threes: 0,
+      twos: 123
+    })
+
+    this.map.set("test4", {
+      doubleGrande: 0,
+      fives: 0,
+      fours: 0,
+      fullHouse: 0,
+      grande: 0,
+      ones: 1,
+      poker: 0,
+      sixes: 0,
+      street: 0,
+      sum: 0,
+      threes: 0,
+      twos: 123
+    })
   }
 
   ngOnInit() {
-      this.sum = this.getSums()[0];
   }
 
   @Input() map!: Map<string, PointsField>;
-  @Input() belegt: boolean[] = [true, false, true]
-
   @Input() readonlyV: boolean = true;
-  @Input() playersField: boolean = false;
-
   @Input() bools: boolean[] = [];
 
-  sum: number = 0;
+  list: string[] = ["ones","twos", "threes", "fours", "fives", "sixes", "fullHouse", "street", "poker", "grande", "doubleGrande"];
+  nameCol: string[] = ["Poker", "9", "10", "B", "D", "K", "A", "F", "St", "P", "G", "Dg", "Summe"];
 
-  list: string[] = ["ones", "twos", "threes", "fours", "fives", "sixes", "fullHouse", "street", "poker", "grande", "doubleGrande"];
-  names: string[] = ["9", "10", "B", "D", "K", "A", "F", "St", "P", "G", "DG"];
+  getPlayers() {
+    let players: string[] = [];
 
-  getRowElements(index: number): number[] {
-    const values: number[] = [];
-
-    for (const obj of this.map.values()) {
-      const objValues = Object.values(obj);
-      const value = objValues[index];
-      if (Number.isInteger(value)) {
-        values.push(value);
-      }
+    for (const obj of this.map.keys()) {
+      players.push(obj);
     }
+
+    return players
+  }
+
+  getValuesOfPlayer(player: string) {
+    let values: number[] = [];
+
+    values.push(this.map.get(player)?.ones!);
+    values.push(this.map.get(player)?.twos!);
+    values.push(this.map.get(player)?.threes!);
+    values.push(this.map.get(player)?.fours!);
+    values.push(this.map.get(player)?.fives!);
+    values.push(this.map.get(player)?.sixes!);
+    values.push(this.map.get(player)?.fullHouse!);
+    values.push(this.map.get(player)?.street!);
+    values.push(this.map.get(player)?.poker!);
+    values.push(this.map.get(player)?.grande!);
+    values.push(this.map.get(player)?.doubleGrande!);
 
     return values;
   }
 
-  getSums() {
-    let sums = []
-
-    for (let key of this.map.keys()) {
-      sums.push(this.map.get(key)!.sum);
-    }
-
-    return sums;
-  }
-
-  add(val: string) {
-    this.sum = this.getSums()[0] + parseInt(val);
-  }
-
-  reset() {
-    this.sum = this.getSums()[0];
-  }
-
-  getNames(): string[] {
-    let list = ["WürfelPoker"];
-
-    for (let key of this.map.keys()) {
-      list.push(key);
-    }
-
-    return list;
+  getSumOfPlayer(player: string) {
+    return this.map.get(player)?.sum!;
   }
 
   click(i: number): void {
     if (!this.readonlyV) {
-      this.routerService.sendValue(this.list[i]);
+      if (this.bools[i]) {
+        this.routerService.sendValue(this.list[i]);
+      }
     }
   }
 
-  protected readonly parseInt = parseInt;
 }
